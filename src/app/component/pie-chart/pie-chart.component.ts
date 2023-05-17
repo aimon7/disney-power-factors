0e35ddbed964694195395aad9edf9df9eb1080bb
Ioannis Antoniou <ioannis@iantoniou.gr>
2023-05-17 14:21:53 +0300

    ✨ Add xlsx functionality to pie chart

--- src/app/component/pie-chart/pie-chart.component.ts
+++ src/app/component/pie-chart/pie-chart.component.ts
@@ -1,7 +1,14 @@
 import { Component, Input } from '@angular/core';
 import * as Highcharts from 'highcharts';
+import exporting from 'highcharts/modules/exporting';
+import exportData from 'highcharts/modules/export-data';
+import * as XLSX from 'xlsx';
 import { HighchartsChartModule } from 'highcharts-angular';
 import { IDisneyCharacter } from 'src/app/characters/characters.component';
+
+exporting(Highcharts);
+exportData(Highcharts);
+
 
 @Component({
   selector: 'app-pie-chart',
@@ -78,6 +85,30 @@ export class PieChartComponent {
         cursor: 'pointer',
         showInLegend: true,
       }
+    },
+    exporting: {
+      enabled: true,
+      csv: {
+        dateFormat: '%Y-%m-%d %H:%M:%S',
+      },
+      buttons: {
+        contextButton: {
+          text: 'Export to XLSX',
+          onclick: function () {
+            const chart = this as unknown as Highcharts.Chart;
+            const data = chart.getCSV(true);
+            const workbook = XLSX.utils.book_new();
+            const worksheet = XLSX.utils.aoa_to_sheet(data.split('\n').map((row) => {
+              if (row.includes('Category')) {
+                return row.replace('Category', 'Character Name').split(',')
+              }
+              return row.split(',')
+            }));
+            XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+            XLSX.writeFile(workbook, 'disney-characters.xlsx');
+          },
+        },
+      },
     },
   }
 }
